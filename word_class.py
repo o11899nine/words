82bd9d9dad9ac935bc501a16339b106c92e1b116
Marcel W <112943380+o11899nine@users.noreply.github.com>
2026-07-13 10:36:09 +0200

    improve

--- word_class.py
+++ word_class.py
@@ -4,11 +4,24 @@ class Word:
     def __init__(self, string: str):
         self.string: str = string
 
-    def strip(self):
-        self.string = self.string.strip()
+    def strip_special_characters(self):
         self.string = self.string.strip("!\",#$%&_£'*-+./")
-    
+
     def remove_accents(self):
         self.string = unidecode(self.string)
 
+    def is_only_caps(self):
+        return self.string.is_upper()
+
+    def is_only_consonants(self):
+        consonants = set("bcdfghjklmnpqrstvwxyz")
+        return all(char in consonants for char in self.string.lower())
+
+    def is_only_same_letter(self):
+        return all(char.lower() == self.string[0].lower() for char in self.string)
+
+    def contains_digit(self):
+        return any(char.isdigit() for char in self.string)
     
+    def length(self):
+        return len(self.string)
--- words_filter.py
+++ words_filter.py
@@ -1,30 +1,40 @@
 import sys
-import word_class
+from word_class import Word
 from enum import Enum
 from collections import defaultdict
-from unidecode import unidecode
 import os
 import math
 
-unique_words: set = set()
-invalid_words: defaultdict = defaultdict(list)
-num_words: int = 0
-num_valid_words: int = 0
-num_invalid_words: int = 0
+invalid_strings_by_reason: defaultdict = defaultdict(list)
+valid_strings: set[str] = set()
 
+num_lines_handled: int = 0
+num_lines: int = 0
+num_invalid_strings: int = 0
+
+# Settings
+SORT_ALPHABETICAL: bool = True
+
+STRIP_SPECIAL_CHARACTERS: bool = True
+REMOVE_ACCENTS: bool = False
+
+MIN_WORD_LENGTH: int = 1
+DENY_ONLY_CAPS: bool = False
+DENY_ONLY_CONSONANTS: bool = False
+DENY_ONLY_SAME_LETTER: bool = False
+DENY_CONTAINS_DIGIT: bool = False
 
 class DenyReason(Enum):
-    NOT_ALPHA = "NOT ALPHA"
-    ALL_CAPS = "ALL CAPS"
-    ALL_CONSONANTS = "ALL CONSONANTS"
-    SINGLE_LETTER = "SINGLE LETTER"
-    DUPLICATE = "DUPLICATE"
-    ALL_SAME_LETTER = "ALL SAME LETTER"
+    TOO_SHORT = "TOO SHORT"
+    ONLY_CAPS = "ONLY CAPS"
+    ONLY_CONSONANTS = "ONLY CONSONANTS"
+    ONLY_SAME_LETTER = "ONLY SAME LETTER"
+    CONTAINS_DIGIT = "CONTAINS DIGIT"
 
 
 def main() -> None:
-    global num_words
-    global num_valid_words
+    global num_lines_handled
+   
 
     if not 3 <= len(sys.argv) <= 4:
         print(
@@ -45,131 +55,122 @@ def main() -> None:
             )
             sys.exit()
 
-    # 1. Read lines once into memory so we can get the length AND loop over them
-    with open(source_filepath, "r", encoding="utf-8") as source_file:
-        words = source_file.readlines()
+    unique_lines: set[str] = get_unique_lines_from_file(source_filepath)
+    num_lines = len(unique_lines)
 
-    source_file_length: int = len(words)
 
-    with open(target_filepath, "w", encoding="utf-8") as target_file:
-        for word in words:
+    for line in unique_lines:
 
-            # Stop at word limit
-            if num_words >= word_limit:
-                break
+        # Stop at word limit
+        if num_lines_handled >= word_limit:
+            break
 
-            # Show progress
-            num_words += 1
-            progress_percentage = round((num_words / source_file_length) * 100, 2)
-            print(f"{progress_percentage}%")
+        # Show progress
+        num_lines_handled += 1
+        progress_percentage = round((num_lines_handled / num_lines) * 100, 2)
+        print(f"{progress_percentage}%", end="\r")
 
-            # Clean word: strip word and remove accents
-            word = unidecode(word.strip())
+        word = Word(line.strip())
 
-            # Skip empty lines
-            if not word:
-                continue
+        if STRIP_SPECIAL_CHARACTERS:
+            word.strip_special_characters()
 
-            # Skip duplicates
-            if word in unique_words:
-                log_invalid_word(word, DenyReason.DUPLICATE)
-                continue
+        if REMOVE_ACCENTS:
+            word.remove_accents()
 
-            # Add word to unique_words for duplicate checking
-            unique_words.add(word)
+        if word.length() < MIN_WORD_LENGTH:
+            log_invalid_word(word, DenyReason.TOO_SHORT)
+            continue
 
-            # Skip single-letter words
-            if len(word) == 1:
-                log_invalid_word(word, DenyReason.SINGLE_LETTER)
-                continue
+        if DENY_ONLY_CAPS and word.is_only_caps():
+            log_invalid_word(word, DenyReason.ONLY_CAPS)
+            continue
 
-            # Skip words with non-alpha characters
-            if not word.isalpha():
-                log_invalid_word(word, DenyReason.NOT_ALPHA)
-                continue
+        if DENY_ONLY_CONSONANTS and word.is_only_consonants():
+            log_invalid_word(word, DenyReason.ONLY_CONSONANTS)
+            continue
 
-            # Skip words in all caps (abbreviations probably)
-            if word.isupper():
-                log_invalid_word(word, DenyReason.ALL_CAPS)
-                continue
+        if DENY_ONLY_SAME_LETTER and word.is_only_same_letter():
+            log_invalid_word(word, DenyReason.ONLY_SAME_LETTER)
+            continue
 
-            # Skip words that are all consonants
-            if is_all_consonants(word):
-                log_invalid_word(word, DenyReason.ALL_CONSONANTS)
-                continue
+        if DENY_CONTAINS_DIGIT and word.contains_digit():
+            log_invalid_word(word, DenyReason.CONTAINS_DIGIT)
+            continue
 
-            # Skip words that consists of all the same letter ('aa' or 'bbb')
-            if is_all_same_letter(word):
-                log_invalid_word(word, DenyReason.ALL_SAME_LETTER)
-                continue
+        # SUCCESS! Word is valid!
+        valid_strings.add(word.string + "\n") # Add \n for fast writelines() later
 
-            # SUCCESS! Word is valid!
-            num_valid_words += 1
-            unique_words.add(word)
-            target_file.write(word + "\n")
-
+    if SORT_ALPHABETICAL:
+        valid_strings = sorted(valid_strings)
+    else:
+        valid_strings = list(valid_strings)
+        
+    write_list_to_file(sorted(valid_strings), target_filepath)
     print("------------------")
     show_totals()
     print("------------------")
-    show_num_invalid_words_per_reason()
+    show_num_invalid_strings_per_reason()
 
     write_to_log()
 
 
-def is_all_same_letter(word: str) -> bool:
-    for i in range(len(word) - 1):
-        if word[i] != word[i + 1]:
-            return False
-    return True
+def write_list_to_file(list: list, filepath: str) -> None:
+    print(f"Writing set to '{filepath}' ...")
+    with open(filepath, "w", encoding="utf-8") as file:
+        file.writelines(list)
+    print("Done!")
 
 
-def clear_terminal() -> None:
-    os.system("cls" if os.name == "nt" else "clear")
+def get_unique_lines_from_file(filepath: str) -> set[str]:
+    # Read all lines
+    print(f"Reading lines from '{filepath}' ...")
+    with open(filepath, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+    print(f"'{filepath}' contains {format_large_int(len(lines))} lines.")
+
+    # Extract unique lines
+    print(f"Extracting unique lines ...")
+    unique_lines = set(lines)
+    print(f"Done! {format_large_int(len(unique_lines))} unique lines found.")
+
+    return unique_lines
 
 
 def show_totals() -> None:
-    print(f"Total words: {format_large_int(num_words)}")
-    print(f"Valid words: {format_large_int(num_valid_words)}")
-    print(f"Invalid words: {format_large_int(num_invalid_words)}")
+    print(f"Lines handled: {format_large_int(num_lines_handled)}")
+    print(f"Valid words found: {format_large_int(len(valid_strings))}")
 
 
-def format_large_int(number: int) -> str:
+def format_large_int(number: int, sep: str = ".") -> str:
     return f"{number:,}".replace(",", ".")
 
 
-def show_num_invalid_words_per_reason() -> None:
-    for reason in invalid_words:
-        num_invalid_words: int = len(invalid_words[reason])
-        print(f"INVALID [{reason.value}]: {format_large_int(num_invalid_words)} words")
+def show_num_invalid_strings_per_reason() -> None:
+    for reason in invalid_strings_by_reason:
+        num_invalid_strings: int = len(invalid_strings_by_reason[reason])
+        print(f"INVALID [{reason.value}]: {format_large_int(num_invalid_strings)} words")
 
 
-def log_invalid_word(word: str, reason: DenyReason) -> None:
-    global num_invalid_words
-    invalid_words[reason].append(word)
-    num_invalid_words += 1
+def log_invalid_word(word: Word, reason: DenyReason) -> None:
+    global num_invalid_strings
+    invalid_strings_by_reason[reason].append(word.string)
+    num_invalid_strings += 1
 
 
 def write_to_log() -> None:
     with open("log.txt", "w", encoding="utf-8") as log:
-        for reason in invalid_words:
+        for reason in invalid_strings_by_reason:
             log.write(
                 "----------------------------------------------------------------------------\n"
             )
-            log.write(f"{reason.value} ({len(invalid_words[reason])} words) \n")
+            log.write(f"{reason.value} ({len(invalid_strings_by_reason[reason])} words) \n")
             log.write(
                 "----------------------------------------------------------------------------\n"
             )
-            for word in invalid_words[reason]:
+            for word in invalid_strings_by_reason[reason]:
                 log.write(word + "\n")
             log.write("\n")
-
-
-def is_all_consonants(word: str) -> bool:
-    if not word:
-        return False
-
-    consonants = set("bcdfghjklmnpqrstvwxyz")
-    return all(character in consonants for character in word.lower())
 
 
 if __name__ == "__main__":
